dc2497effb477a32ee25e9f0cd643fc531bc6959
Jignesh Jain <jigneshjain25@gmail.com>
2014-12-01 18:27:23 +0530

    small change in modular expo func

--- number_theory.h
+++ number_theory.h
@@ -19,7 +19,7 @@ long long ncr(int n,int r){
  *  .. where P is the no mod which we want inverse
  */
 int modulo(int a,int b,int c){
-    long long x=1,y=a; 
+    long long x=1,y=a%c; 
     while(b > 0){
         if(b%2 == 1){
             x=(x*y)%c;
