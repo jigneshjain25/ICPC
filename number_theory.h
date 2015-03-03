2f21a08c9cfb3164c027c3e5a3505815b2fc394e
Jignesh Jain <jigneshjain25@gmail.com>
2015-03-04 01:17:31 +0530

    tp

--- number_theory.h
+++ number_theory.h
@@ -86,20 +86,20 @@ namespace sieve{
 void primeFactors(int n)
 {
     // Print the number of 2s that divide n
-    while (n%2 == 0)
+    while(n%2 == 0)
     {
         printf("%d ", 2);
-        n = n/2;
+        n /= 2;
     }
  
     // n must be odd at this point.  So we can skip one element (Note i = i +2)
-    for (int i = 3; i <= sqrt(n); i = i+2)
+    for(int i=3; i*i<=n; i+=2)
     {
         // While i divides n, print i and divide n
         while (n%i == 0)
         {
             printf("%d ", i);
-            n = n/i;
+            n /= i;
         }
     }
  
@@ -157,3 +157,16 @@ A = i + b/2 -1
 
 a/b mod m = (a mod bm) / b
 // helps when m and b are not coprime
+
+// mobius function
+// μ(n) = 1 if n is a square-free positive integer with an even number of prime factors.
+// μ(n) = −1 if n is a square-free positive integer with an odd number of prime factors.
+// μ(n) = 0 if n has a squared prime factor.
+
+int mob[maxM];
+void computeMob(int maxM) {
+    memset(mob,0,sizeof mob); mob[1]=1;
+    for(int i=1;i<=maxM;++i)
+        for(int j=2*i;j<=maxM;j+=i)
+            mob[j] -= mob[i];
+}
